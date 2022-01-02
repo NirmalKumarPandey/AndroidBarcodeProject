4c9fcf16beaf9b620ba9a50c8b879bb6399dee6a
Nirmal Kumar Pandey <nirmalnirman36@gmail.com>
2022-01-02 10:18:15 -0500

    added import

--- app/src/main/java/com/example/barcodegeneratorwithimage/MainActivity.java
+++ app/src/main/java/com/example/barcodegeneratorwithimage/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
